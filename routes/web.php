31518f4fde721da93f548ca5735c5784ee879a1e
kholoud001 <kholoud.sanak@gmail.com>
2024-06-24 14:31:52 +0100

    update homepage

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        return view('dashboard');
+    } else {
+        return view('auth.login');
+    }
 });
 
 Route::get('/dashboard', function () {
